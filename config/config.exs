3c869622419acd2e86c07e689024e2e724279be8
Christoph Lupprich <christoph@luppri.ch>
2025-09-27 21:35:42 +0200

    Copy over default fwup.conf and config.txt

--- config/config.exs
+++ config/config.exs
@@ -58,6 +58,7 @@ config :darker, target: Mix.target()
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+config :nerves, :firmware, fwup_conf: "config/fwup.conf"
 
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
 # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
